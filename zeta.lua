8025da405a38411c9eb24a89198bd2f22cd8c025
Renan <167133780+zDevNan@users.noreply.github.com>
2025-02-22 15:08:56 -0300

    Create zeta.lua

---
+++ zeta.lua
@@ -0,0 +1,31 @@
+--Data Printer teste
+if game.PlaceId == 3237168 then
+
+    local player = game.Players.NOMEPLAYER -- trocar pelo nome do player
+    local userData = game.Workspace.UserData:FindFirstChild("User_"..player.UserId)
+    local Data = userData.Data
+    print(player.userId)
+    print("Name:", player.Name)
+    print("Beri:", Data.Cash.Value)
+    print("Bounty:", Data.Bounty.Value)
+    print("Compasses:", Data.CompassTokens.Value)
+    print("Gems:", Data.Gems.Value)
+    print("Kills:", Data.Kills.Value)
+    print("DF1:", Data.DevilFruit.Value)
+    print("DF2:", Data.DevilFruit2.Value)
+    print("StoredDF1:", Data.StoredDF1.Value)
+    print("StoredDF2:", Data.StoredDF2.Value)
+    print("StoredDF3:", Data.StoredDF3.Value)
+    print("StoredDF4:", Data.StoredDF4.Value)
+    print("StoredDF5:", Data.StoredDF5.Value)
+    print("StoredDF6:", Data.StoredDF6.Value)
+    print("StoredDF7:", Data.StoredDF7.Value)
+    print("StoredDF8:", Data.StoredDF8.Value)
+    print("StoredDF9:", Data.StoredDF9.Value)
+    print("StoredDF10:", Data.StoredDF10.Value)
+    print("StoredDF11:", Data.StoredDF11.Value)
+    print("StoredDF12:", Data.StoredDF12.Value)
+
+
+
+    end
