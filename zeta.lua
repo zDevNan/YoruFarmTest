8ca0802de6c613debd9167bf2f9e3c21e319c087
Renan <167133780+zDevNan@users.noreply.github.com>
2025-02-22 15:13:21 -0300

    Update zeta.lua

--- zeta.lua
+++ zeta.lua
@@ -1,31 +1,34 @@
---Data Printer teste
+-- Data Printer Teste
 if game.PlaceId == 3237168 then
+    local playerId = 4630197578 -- Substitua pelo ID do jogador desejado
+    local player = game.Players:GetPlayerByUserId(playerId)
 
-    local player = game.Players.Luffy107_99 -- trocar pelo nome do player
-    local userData = game.Workspace.UserData:FindFirstChild("User_"..player.UserId)
-    local Data = userData.Data
-    print(player.userId)
-    print("Name:", player.Name)
-    print("Beri:", Data.Cash.Value)
-    print("Bounty:", Data.Bounty.Value)
-    print("Compasses:", Data.CompassTokens.Value)
-    print("Gems:", Data.Gems.Value)
-    print("Kills:", Data.Kills.Value)
-    print("DF1:", Data.DevilFruit.Value)
-    print("DF2:", Data.DevilFruit2.Value)
-    print("StoredDF1:", Data.StoredDF1.Value)
-    print("StoredDF2:", Data.StoredDF2.Value)
-    print("StoredDF3:", Data.StoredDF3.Value)
-    print("StoredDF4:", Data.StoredDF4.Value)
-    print("StoredDF5:", Data.StoredDF5.Value)
-    print("StoredDF6:", Data.StoredDF6.Value)
-    print("StoredDF7:", Data.StoredDF7.Value)
-    print("StoredDF8:", Data.StoredDF8.Value)
-    print("StoredDF9:", Data.StoredDF9.Value)
-    print("StoredDF10:", Data.StoredDF10.Value)
-    print("StoredDF11:", Data.StoredDF11.Value)
-    print("StoredDF12:", Data.StoredDF12.Value)
+    if player then
+        local userData = game.Workspace.UserData:FindFirstChild("User_" .. player.UserId)
 
+        if userData and userData:FindFirstChild("Data") then
+            local Data = userData.Data
+            print("User ID:", player.UserId)
+            print("Name:", player.Name)
+            print("Beri:", Data.Cash.Value)
+            print("Bounty:", Data.Bounty.Value)
+            print("Compasses:", Data.CompassTokens.Value)
+            print("Gems:", Data.Gems.Value)
+            print("Kills:", Data.Kills.Value)
+            print("DF1:", Data.DevilFruit.Value)
+            print("DF2:", Data.DevilFruit2.Value)
 
-
+            -- Impressão das frutas armazenadas (StoredDF1 a StoredDF12)
+            for i = 1, 12 do
+                local storedDF = Data:FindFirstChild("StoredDF" .. i)
+                if storedDF then
+                    print("StoredDF" .. i .. ":", storedDF.Value)
+                end
+            end
+        else
+            warn("UserData não encontrado para o jogador:", player.Name)
+        end
+    else
+        warn("Jogador com ID " .. playerId .. " não encontrado.")
     end
+end
